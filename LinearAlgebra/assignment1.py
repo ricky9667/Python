95cf75f5ccfa8ae98bd7c5ffdfb267c10079c546
Ricky Hu <ricky9667@gmail.com>
2020-03-05 14:58:49 +0800

    implement inverse function, finis pC

--- LinearAlgebra/assignment1.py
+++ LinearAlgebra/assignment1.py
@@ -4,6 +4,8 @@ matrix_a = [[-1, 2, 0], [2, 0, 3]]
 matrix_b = [[2, 0, -1], [1, -2, 0]]
 matrix_c = [[0, 2], [1, 0], [-1, 1]]
 matrix_e = [[2, -1], [math.pi, math.log10(2)], [-2, 6]]
+matrix_f = []
+matrix_g = []
 matrix_i = [[1, 0], [0, 1]]
 
 def matrix_add(matrix_x, pow_x, matrix_y, pow_y):
@@ -24,16 +26,16 @@ def matrix_add(matrix_x, pow_x, matrix_y, pow_y):
         matrix_sum.append(temp)
     return matrix_sum
 
-def matrix_transpose(matrix_input):
-    rows = len(matrix_input)
-    cols = len(matrix_input[0])
-    matrix_output = []
+def matrix_transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    output = []
     for i in range(cols):
         temp = []
         for j in range(rows):
-            temp.append(matrix_input[j][i])
-        matrix_output.append(temp)
-    return matrix_output
+            temp.append(matrix[j][i])
+        output.append(temp)
+    return output
 
 def matrix_mutiply(matrix_x, matrix_y):
     rows_x = len(matrix_x)
@@ -55,6 +57,12 @@ def matrix_mutiply(matrix_x, matrix_y):
         matrix_mul.append(row)
     return matrix_mul
 
+def matrix_inverse(matrix):
+    p = matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+    print(p)
+    output = [[matrix[1][1]/p, -matrix[0][1]/p], [-matrix[1][0]/p, matrix[0][0]/p]]
+    return output
+
 def problem_a():
     global matrix_a, matrix_b, matrix_c, matrix_e
     print("\n========= Problem A =========\n")
@@ -65,19 +73,25 @@ def problem_a():
     print("C - E =", a2)
     print("A ^ T =", matrix_transpose(matrix_a))
     print("E ^ T =", matrix_transpose(matrix_e))
-    
-    print("\n=============================\n")
 
 def problem_b():
-    global matrix_a, matrix_c
+    global matrix_a, matrix_c, matrix_f
     print("\n========= Problem B =========\n")
-    print("F = A * C =", matrix_mutiply(matrix_a, matrix_c))
-    print("F = C * A =", matrix_mutiply(matrix_c, matrix_a))
-    print("\n=============================\n")
+    matrix_f = matrix_mutiply(matrix_a, matrix_c)
+    print("F = A * C =", matrix_f)
+    matrix_g = matrix_mutiply(matrix_c, matrix_a)
+    print("G = C * A =", matrix_g)
+
+def problem_c():
+    global matrix_f
+    print("\n========= Problem C =========\n")
+    print("F ^ (-1) =", matrix_inverse(matrix_f))
 
 def main():
     problem_a()
     problem_b()
+    problem_c()
+    print()
 
 if __name__ == "__main__":
     main()
